f2b06acc593212ecdb194bbe888b8dcc8f21fe42
geraki <falcon2981@gmail.com>
2013-10-29 18:33:06 +0400

    all ready lets go

---
+++ HAL.h
@@ -0,0 +1,22 @@
+#ifndef HAL_H_
+#define HAL_H_
+
+// железо
+// atmega8
+//#define ATMEGA8
+
+// atmega328p
+#define ATMEGA328
+
+
+// инициализация для 8
+#ifdef ATMEGA8
+
+#endif // ATMEGA8
+
+// инициализация для 328
+#ifdef ATMEGA328
+
+#endif // ATMEGA328
+
+#endif // HAL_H_
