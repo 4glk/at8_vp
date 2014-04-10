db2903c3ec3aa939aef16522278a9ed6580c9d35
Geraki <falcon2981@gmail.com>
2014-04-10 11:46:23 +0400

    gitignore tweak

--- main.c
+++ main.c
@@ -67,7 +67,7 @@ int main(void)
     nlcd_Init();
     MenuInit();
     InitBurner();
-///*
+/*
 *  This program is free software: you can redistribute it and/or modify
 *  it under the terms of the GNU General Public License as published by
 *  the Free Software Foundation, either version 3 of the License, or
